83610b7ea267ec7d2f59851134ea31056b2fe486
liujunhui1 <748936836@qq.com>
2019-07-10 18:31:18 +0800

    test

---
+++ src/main/java/cn/zhonggong/makeup/TestController.java
@@ -0,0 +1,18 @@
+package cn.zhonggong.makeup;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 军辉
+ * 2019-07-10 17:07
+ */
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    public ModelAndView test(){
+        return new ModelAndView("test");
+    }
+}
