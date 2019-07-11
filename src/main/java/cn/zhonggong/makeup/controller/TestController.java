69e0b1a986eb1e6da9b817d0c85f88c153f22772
liujunhui1 <748936836@qq.com>
2019-07-11 19:42:25 +0800

    整合了一下资源

--- src/main/java/cn/zhonggong/makeup/controller/TestController.java
+++ src/main/java/cn/zhonggong/makeup/controller/TestController.java
@@ -2,6 +2,7 @@ package cn.zhonggong.makeup.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -12,7 +13,26 @@ import org.springframework.web.servlet.ModelAndView;
 public class TestController {
 
     @GetMapping("/test")
-    public ModelAndView test(){
+    public ModelAndView test() {
+        System.out.println("test");
         return new ModelAndView("test");
+    }
+
+    @PostMapping("/index")
+    public ModelAndView Post_index() {
+        System.out.println("Post_index");
+        return new ModelAndView("index");
+    }
+
+    @GetMapping("/index")
+    public ModelAndView Get_index() {
+        System.out.println("Get_index");
+        return new ModelAndView("index");
+    }
+
+    @GetMapping("/login")
+    public ModelAndView login() {
+        System.out.println("login");
+        return new ModelAndView("login");
     }
 }
